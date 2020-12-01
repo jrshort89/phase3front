016ce538efb508c4ba994b3d661ffb33ea3f9e18
raaynaldo <raaynaldo@hotmail.com>
2020-12-01 15:49:47 -0500

    update git reset, git checkout, git add

--- dist/GitCommand.js
+++ dist/GitCommand.js
@@ -1,86 +1,151 @@
 class GitCommand {
-    constructor() {
-        this.gitButtonSetup();
-        this.gitCommandRunSetup();
+  workingDir = document.getElementById("working-directory-area-list");
+  stagingArea = document.getElementById("staging-area-list");
+  repoArea = document.getElementById("repository-area-list");
+
+  gitAddDotBtn = document.getElementById("git-add-dot");
+  gitCommitBtn = document.getElementById("git-commit");
+  gitResetSoftBtn = document.getElementById("git-reset-soft");
+  gitResetDotBtn = document.getElementById("git-reset-dot");
+  gitCheckoutDotBtn = document.getElementById("git-checkout-dot");
+
+  constructor() {
+    this.gitButtonSetup();
+    this.gitCommandRunSetup();
+  }
+
+  gitButtonSetup() {
+    this.gitAddDotBtn.addEventListener("click", (event) =>
+      this.gitButtonCallBack(event)
+    );
+    this.gitCommitBtn.addEventListener("click", (event) =>
+      this.gitButtonCallBack(event)
+    );
+    this.gitResetSoftBtn.addEventListener("click", (event) =>
+      this.gitButtonCallBack(event)
+    );
+    this.gitResetDotBtn.addEventListener("click", (event) =>
+      this.gitButtonCallBack(event)
+    );
+    this.gitCheckoutDotBtn.addEventListener("click", (event) =>
+      this.gitButtonCallBack(event)
+    );
+  }
+
+  gitButtonCallBack(event) {
+    let gitCommand = document.getElementById("git-command");
+    gitCommand.value = event.target.innerText;
+    gitCommand.focus();
+  }
+
+  gitCommandRunSetup() {
+    let gitCommandForm = document.getElementById("git-command-form");
+    gitCommandForm.addEventListener("submit", (event) =>
+      this.gitCommandRunCallback(event)
+    );
+  }
+
+  async gitCommandRunCallback(event) {
+    event.preventDefault();
+    const command = event.target.git_command.value;
+    const command_split = command.split(" ");
+    //git add change to stage 2
+    if (command_split[0] == "git" && command_split[1] == "add") {
+      let workingDirList = [...this.workingDir.querySelectorAll(".item")];
+      const stagingList = this.stagingArea;
+      if (command_split[2] == "." && workingDirList.length > 0) {
+        const versionIds = workingDirList.map((list) => list.dataset.versionId);
+        await this.updateVerstionStage(versionIds, 2);
+        this.moveListsToOtherArea(workingDirList, stagingList);
+      } else if (
+        workingDirList.find((div) => div.dataset.fileName == command_split[2])
+      ) {
+        const fileDiv = workingDirList.find(
+          (div) => div.dataset.fileName == command_split[2]
+        );
+        const versionId = fileDiv.dataset.versionId;
+        workingDirList = [fileDiv];
+        await this.updateVerstionStage(versionId, 2);
+        this.moveListsToOtherArea(workingDirList, stagingList);
+      }
+      // git reset change to stage 1
+    } else if (command_split[0] == "git" && command_split[1] == "reset") {
+      let stagingList = [...this.stagingArea.querySelectorAll(".item")];
+      let workingDirList = this.workingDir;
+      if (command_split[2] == ".") {
+        const versionIds = stagingList.map((list) => list.dataset.versionId);
+        await this.updateVerstionStage(versionIds, 1);
+        this.moveListsToOtherArea(stagingList, workingDirList);
+      } else if (
+        stagingList.find((div) => div.dataset.fileName == command_split[2])
+      ) {
+        const fileDiv = stagingList.find(
+          (div) => div.dataset.fileName == command_split[2]
+        );
+        const versionId = fileDiv.dataset.versionId;
+        stagingList = [fileDiv];
+        await this.updateVerstionStage(versionId, 1);
+        this.moveListsToOtherArea(stagingList, workingDirList);
+      }
+      // git checkout
+    } else if (command_split[0] == "git" && command_split[1] == "checkout") {
+      let workingDirList = [...this.workingDir.querySelectorAll(".item")];
+      if (command_split[2] == ".") {
+        const versionIds = workingDirList.map((list) => list.dataset.versionId);
+        await this.deleteVerstionStage(versionIds);
+        this.removeListsToOtherArea(workingDirList);
+      } else if (
+        workingDirList.find((div) => div.dataset.fileName == command_split[2])
+      ) {
+        const fileDiv = workingDirList.find(
+          (div) => div.dataset.fileName == command_split[2]
+        );
+        const versionId = fileDiv.dataset.versionId;
+        workingDirList = [fileDiv];
+        await this.deleteVerstionStage(versionId, 1);
+        this.removeListsToOtherArea(workingDirList);
+      }
     }
-    workingDir = document.getElementById("working-directory-area-list");
-    stagingArea = document.getElementById("staging-area-list");
-    repoArea = document.getElementById("repository-area-list");
-    repoList = document.getElementById("repo-options");
 
-    repoForm = document.getElementById('repo-form');
+    event.target.reset();
+  }
 
-    gitAddDotBtn = document.getElementById('git-add-dot');
-    gitCommitBtn = document.getElementById('git-commit');
-    gitResetSoftBtn = document.getElementById('git-reset-soft');
-    gitResetDotBtn = document.getElementById('git-reset-dot');
-    gitCheckoutDotBtn = document.getElementById('git-checkout-dot');
+  updateVerstionStage(versionIds, stage) {
+    const requestURL = "http://localhost:3000/versions/bulk";
+    const data = {
+      versionIds: versionIds,
+      stage: stage,
+    };
+    const requestObj = {
+      method: "PATCH",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
+    };
+    return fetch(requestURL, requestObj);
+  }
 
-    gitButtonSetup() {
-        this.gitAddDotBtn.addEventListener("click", (event) => this.gitButtonCallBack(event))
-        this.gitCommitBtn.addEventListener("click", (event) => this.gitButtonCallBack(event))
-        this.gitResetSoftBtn.addEventListener("click", (event) => this.gitButtonCallBack(event))
-        this.gitResetDotBtn.addEventListener("click", (event) => this.gitButtonCallBack(event))
-        this.gitCheckoutDotBtn.addEventListener("click", (event) => this.gitButtonCallBack(event))
-    }
+  moveListsToOtherArea(fromList, toList) {
+    fromList.forEach(function (div) {
+      toList.appendChild(div);
+    });
+  }
 
-    gitButtonCallBack(event) {
-        let gitCommand = document.getElementById('git-command');
-        gitCommand.value = event.target.innerText
-        gitCommand.focus()
-    }
+  deleteVerstionStage(versionIds) {
+    const requestURL = "http://localhost:3000/versions/bulk";
+    const data = {
+      versionIds: versionIds,
+    };
+    const requestObj = {
+      method: "DELETE",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
+    };
+    return fetch(requestURL, requestObj);
+  }
 
-    gitCommandRunSetup() {
-        let gitCommandForm = document.getElementById("git-command-form");
-        gitCommandForm.addEventListener("submit", (event) => this.gitCommandRunCallback(event))
-    }
-
-    gitCommandRunCallback(event) {
-        event.preventDefault();
-        const command = event.target.git_command.value
-        const command_split = command.split(" ");
-        if (command_split[0] == "git" && command_split[1] == "add") {
-            const workingDirList = [...this.workingDir.querySelectorAll(".item")]
-            let stagingList = this.stagingArea
-            if (command_split[2] == "." && workingDirList.length > 0) {
-                const versionIds = workingDirList.map((list) => list.dataset.versionId)
-                const data = {
-                    versionIds: versionIds,
-                    stage: 2
-                }
-                fetch("http://localhost:3000/versions/bulk", {
-                    method: "PATCH",
-                    headers: { "Content-Type": "application/json" },
-                    body: JSON.stringify(data)
-                }).then(res => res.json()).then(json => {
-                    workingDirList.forEach(function(div) {
-                        stagingList.appendChild(div)
-                    })
-                });
-            } else if (workingDirList.find((div) => div.dataset.fileName == command_split[2])) {
-                stagingList.appendChild(workingDirList.find((div) => div.dataset.fileName == command_split[2]))
-            }
-        } else if (command_split[0] == "git" && command_split[1] == "reset") {
-            const stagingList = [...this.stagingArea.querySelectorAll(".item")]
-            let workingDirList = this.workingDir
-            if (command_split[2] == ".") {
-                stagingList.forEach(function(div) {
-                    workingDirList.appendChild(div)
-                })
-            } else if (stagingList.find((div) => div.dataset.fileName == command_split[2])) {
-                workingDirList.appendChild(stagingList.find((div) => div.dataset.fileName == command_split[2]))
-            }
-        } else if (command_split[0] == "git" && command_split[1] == "checkout") {
-            const workingDirList = [...this.workingDir.querySelectorAll(".item")]
-            if (command_split[2] == ".") {
-                workingDirList.forEach(function(div) {
-                    div.remove()
-                })
-            } else if (workingDirList.find((div) => div.dataset.fileName == command_split[2])) {
-                workingDirList.find((div) => div.dataset.fileName == command_split[2]).remove()
-            }
-        }
-
-        event.target.reset();
-    }
+  removeListsToOtherArea(list) {
+    list.forEach(function (div) {
+      div.remove();
+    });
+  }
 }
